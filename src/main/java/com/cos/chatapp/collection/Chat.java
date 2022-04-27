564900c2361f0c936e448629ff6701695581e40e
BEDongryeol <ehdfuf53@gmail.com>
2022-04-27 22:22:25 +0900

    feat: Add Chat Collection
    - String id [Id]
    - String msg [message]
    - String sender
    - String receiver
    - Integer roomNum
    - LocalDateTime createdAt

--- src/main/java/com/cos/chatapp/collection/Chat.java
+++ src/main/java/com/cos/chatapp/collection/Chat.java
@@ -13,8 +13,10 @@ public class Chat {
     @Id
     private String id;
     private String msg;
-    private String sender;
-    private String receiver;
+    // 채팅은 room에 메세지를 보내는 형식으로 구현하는 것이 편하다.
+    private String sender;   // 보내는 사람
+    private String receiver; // 받는 사람(귓속말)
+    private Integer roomNum; // 방번호
 
     private LocalDateTime createdAt;
 }
--- src/main/java/com/cos/chatapp/repository/ChatRepository.java
+++ src/main/java/com/cos/chatapp/repository/ChatRepository.java
@@ -8,7 +8,13 @@ import reactor.core.publisher.Flux;
 
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 
+    // 귓속말
     @Tailable // 커서를 닫지 않고 유지한다.
     @Query("{sender:?0, receiver:?1}")
     Flux<Chat> mFindBySender(String sender, String receiver);
+
+    // 같은 방에서 채팅
+    @Tailable
+    @Query("{roomNum:?0}")
+    Flux<Chat> mFindByRoomNum(Integer roomNum);
 }
